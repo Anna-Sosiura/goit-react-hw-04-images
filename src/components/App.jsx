09e51c19e94edd418728b3af905dc74d973f5c5c
Anna-Sosiura <132984503+Anna-Sosiura@users.noreply.github.com>
2023-12-19 00:04:08 +0200

    commit-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,94 +1,85 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import { getImages } from '../service/image-service';
 
-import { Searchbar } from './Searchbar/Searchbar';
+import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    total: 0,
-    error: null,
-    loader: false,
-    modalImage: '',
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const [modalImage, setModalImage] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query: prevQuery, page: prevPage } = prevState;
-    const { query, page } = this.state;
-    if (prevQuery !== query || prevPage !== page) {
-      this.getPhotos(query, page);
-    }
-  }
-  getPhotos = async (query, page) => {
-    this.setState({ loader: true });
+  useEffect(() => {
+    if (query !== '') getPhotos(query, page);
+  }, [query, page]);
+  const getPhotos = async (query, page) => {
+    setLoader(true);
+    setError(null);
     try {
       const { hits, total } = await getImages(query, page);
-      this.setState(prevState => ({
-        images: [...prevState.images, ...hits],
-        total: total,
-      }));
+      const images = hits.map(el => {
+        const { id, webformatURL, largeImageURL } = el;
+        const image = { id, webformatURL, largeImageURL };
+        return image;
+      });
+      page > 1
+        ? setImages(prevImages => [...prevImages, ...images])
+        : setImages(images);
+      setTotal(total);
     } catch (error) {
-      this.setState({ error: error.message });
+      setError(error.message);
     } finally {
-      this.setState({ loader: false });
+      setLoader(false);
     }
   };
-  handleSubmit = value => {
-    this.setState({
-      query: value,
-      page: 1,
-      images: [],
-      total: 0,
-      error: null,
-      loading: false,
-    });
+  const handleSubmit = value => {
+    setQuery(value);
+    setPage(1);
+    setImages([]);
+    setTotal(0);
   };
-  onClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const onClick = () => {
+    setPage(page + 1);
   };
-  showmodal = modalImage => {
-    this.setState({ modalImage });
+  const showmodal = modalImage => {
+    setModalImage(modalImage);
   };
-  closemodal = () => {
-    this.setState({ modalImage: '' });
+  const closemodal = () => {
+    setModalImage('');
   };
 
-  render() {
-    const { images, total, error, loader, query, modalImage } = this.state;
-
-    console.log(images);
-    return (
-      <>
-        <Searchbar className="Searchbar" onSubmit={this.handleSubmit} />
-        {error && <h3>Sorry. There is an error {error} 😭</h3>}
-        {query !== '' && total === 0 && (
-          <h3>Sorry. There are not images in request {query}</h3>
-        )}
-        {loader ? (
-          // <Loader>Loading...</Loader>
-          <h3>Loading...</h3>
-        ) : (
-          <>
-            <ImageGallery
-              className="ImageGallery"
-              images={images}
-              showmodal={this.showmodal}
-            />
-            {modalImage && (
-              <Modal largeImage={modalImage} closemodal={this.closemodal} />
-            )}
-            {total > images.length && <Button onClick={this.onClick}></Button>}
-          </>
-        )}
-      </>
-    );
-  }
-}
+  console.log(images);
+  return (
+    <>
+      <Searchbar className="Searchbar" onSubmit={handleSubmit} />
+      {error && <h3>Sorry. There is an error {error} 😭</h3>}
+      {query !== '' && total === 0 && (
+        <h3>Sorry. There are not images in request {query}</h3>
+      )}
+      {loader ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          (
+          <ImageGallery
+            className="ImageGallery"
+            images={images}
+            showmodal={showmodal}
+          />
+          )
+          {modalImage && (
+            <Modal largeImage={modalImage} closemodal={closemodal} />
+          )}
+          {total > images.length && <Button onClick={onClick}></Button>}
+        </>
+      )}
+    </>
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,43 +1,36 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class Searchbar extends Component {
-  state = {
-    query: '',
+const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setQuery(value);
   };
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state.query.toLowerCase().trim());
-    this.setState({ query: '' });
+    onSubmit(query.toLowerCase().trim());
+    setQuery('');
   };
-  render() {
-    const {
-      state: { query },
-      handleChange,
-      handleSubmit,
-    } = this;
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={handleSubmit}>
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
 
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="query"
-            value={query}
-            onChange={handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="query"
+          value={query}
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
+};
+export default Searchbar;
